4e00b7d4103e26c1ee001d8d60844d0f46ea4629
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-12 20:48:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TransportBuilder
+{
+	public class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Director director = new Director();
+			Builder carBuilder = new CarBuilder();
+			director.Construct(carBuilder);
+
+			carBuilder.GetProduct().PrintInfo();
+
+			Builder motorBuilder = new MotorBuilder();
+			director.Construct(motorBuilder);
+
+			motorBuilder.GetProduct().PrintInfo();
+
+			Console.ReadKey();
+
+		}
+	}
+
+}
