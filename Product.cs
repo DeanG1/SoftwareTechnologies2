7b59da7173451253e5bf4d8762a372eec906e869
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-12 20:47:59 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportBuilder
+{
+    class Product
+    {
+        public string Frame { get; set; }
+        public string Engine { get; set; }
+        public int Doors { get; set; }
+        public int Wheels { get; set; }
+
+        public void PrintInfo() 
+        {
+            Console.WriteLine("Frame: {0}", Frame);
+            Console.WriteLine("Engine: {0}", Engine);
+            Console.WriteLine("Doors: {0}", Doors);
+            Console.WriteLine("Wheels: {0}", Wheels);
+            
+        }
+
+    }
+}
