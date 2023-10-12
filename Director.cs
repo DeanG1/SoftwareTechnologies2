f45589d6b0a510b1b89f170d39843698499f5330
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-12 20:47:15 +0300

    Add files via upload

---
+++ Director.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportBuilder
+{
+     class Director
+    {
+        public void Construct(Builder builder) 
+        {
+            builder.BuildFrame();
+            builder.BuildEngine();
+            builder.BuildDoors();
+            builder.BuildWheels();
+
+        }
+    }
+}
