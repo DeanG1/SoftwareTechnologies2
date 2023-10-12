a4b67534f1a7f273f36b7ccec5958665b2f2b6a8
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-12 20:47:25 +0300

    Add files via upload

---
+++ MotorBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportBuilder
+{
+    class MotorBuilder: Builder  //ConcreteBuilder2
+    {
+        public Product product = new Product();
+        public override void BuildFrame()
+        {
+            product.Frame = "Motor frame";
+        }
+        public override void BuildEngine()
+        {
+            product.Engine = "50 cc";
+        }
+        public override void BuildDoors()
+        {
+            product.Doors = 0;
+        }
+        public override void BuildWheels()
+        {
+            product.Wheels = 2;
+        }
+        public override Product GetProduct()
+        {
+            return product; 
+        }
+    }
+}
