90480ac3851373d51fe64eaa621f4851eaf344b4
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-17 19:30:10 +0300

    Add files via upload

---
+++ EuropeFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyotaAbstractFactory
+{
+     class EuropeFactory: ToyotaFactory // Concrete Factory 2
+        {
+        public override Toyota CreateModel() 
+        {
+            return new CorollaToyota();   //Concrete Product2
+
+        }
+    }
+}
