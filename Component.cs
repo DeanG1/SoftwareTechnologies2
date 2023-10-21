d2c460cc7059734fb1dcd2289743298c2bfa8cab
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-21 21:34:27 +0300

    Add files via upload

---
+++ Component.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeStructure
+{
+    abstract class Component
+    {
+        public abstract string Operation();
+        
+
+        public virtual void Add(Component component) 
+        {
+            throw new NotImplementedException();    
+        }
+        public virtual void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual bool IsComposite() 
+        {
+            return true;
+        }
+    }
+}
