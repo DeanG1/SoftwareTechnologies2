200cd5894c1d337e6b4b063ef0a3b6905af3c7f7
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-24 09:27:51 +0300

    Add files via upload

---
+++ Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksAdapter
+{
+    class Library
+    {
+        protected string BookId;
+        protected string nameOfBook;
+        protected string author;
+        protected string genre;
+        protected string year;
+
+        public Library(string bookId) 
+        {
+            this.BookId = bookId;   
+        }
+        public virtual void Display() 
+        {
+            Console.WriteLine("\nBook: {0} ------", BookId);
+        }
+
+
+    }
+}
