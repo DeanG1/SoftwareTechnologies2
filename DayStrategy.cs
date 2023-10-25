0c9a85cf5d55765fed29d75eb29a1a49f1093f1c
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-25 19:47:33 +0300

    Add files via upload

---
+++ DayStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyNightBar
+{
+    public class DayStrategy : NightBarStrategy
+    {
+        public override void Calculate(double amount)
+        {
+            Console.WriteLine(amount / 2 + "paid by DayStrategy");
+        }
+    }
+}
