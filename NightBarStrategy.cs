7a17a9775e5124f04b05b60ae5e5d06a4a35e42c
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-25 19:47:52 +0300

    Add files via upload

---
+++ NightBarStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyNightBar
+{
+    public abstract class NightBarStrategy
+    {
+        public abstract void Calculate(double amount);
+
+
+    }
+}
