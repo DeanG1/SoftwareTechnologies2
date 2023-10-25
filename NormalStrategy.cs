d4f88ade082f160bd80f57b360dff5c87eed112b
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-25 19:48:05 +0300

    Add files via upload

---
+++ NormalStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyNightBar
+{
+    public class NormalStrategy:NightBarStrategy  //ConcreteStrategy1
+    {
+        public override void Calculate(double amount)
+        {
+            Console.WriteLine(amount + " paid by NormalStrategy");
+        }
+
+    }
+}
