67046a97c3dbed5308da351e420ce59a57c67fa8
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-28 19:02:44 +0300

    Add files via upload

---
+++ Vegetable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegetablePriceobserver
+{
+    abstract class Vegetable
+    {
+        private string _vegetableName;
+        private double _price;
+        private List<IRestaurant> _restaurants = new List<IRestaurant>();   
+
+        //Constructor
+
+        public Vegetable(string vegetableName, double price) 
+        {
+            this._vegetableName = vegetableName;
+            this._price = price;
+        }
+        public void Attach(IRestaurant restaurant) 
+        {
+            _restaurants.Add(restaurant);   
+        }
+        public void Detach(IRestaurant restaurant)
+        {
+            _restaurants.Remove(restaurant);
+        }
+
+        public void Notify() 
+        {
+            foreach (IRestaurant restaurant in _restaurants) 
+            {
+                restaurant.Update(this);
+            }
+            Console.WriteLine("");
+        }
+        public double price 
+        {
+            get { return _price; }
+            set 
+            {
+                if (_price != value) 
+                {
+                    _price = value;
+                    Notify();   
+                }
+            }
+
+        }
+        public string VegetableName
+        {
+            get { return _vegetableName; }
+        }
+    }
+}
