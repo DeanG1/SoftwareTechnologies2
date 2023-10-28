0f312f7df77ddc85f3505ff0101abc7ca9723744
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-28 19:02:22 +0300

    Add files via upload

---
+++ Tomato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegetablePriceobserver
+{
+    class Tomato: Vegetable //ConcreteSubject1
+    {
+        public Tomato(string vegetableName, double price) 
+            :base (vegetableName, price) 
+        {
+            
+        }
+            
+    }
+}
