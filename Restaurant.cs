ae4ad25a06075a42ec47e8094a5e8c9a0fcb33e7
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-28 19:02:33 +0300

    Add files via upload

---
+++ Restaurant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegetablePriceobserver
+{
+    class Restaurant: IRestaurant  // ConcreteRestaurant
+    {
+        private string _name;
+        private Vegetable _vegetable;
+
+        //Constructor
+        public Restaurant(string name) 
+        {
+            this._name = name;
+        }
+
+        public void Update(Vegetable vegetable) 
+        {
+            Console.WriteLine("Notifies restaurant {0} that the price of " + "{1} is changed to {2} BGN", _name, vegetable.VegetableName, vegetable.price);
+
+
+        }
+        //Wzema  ili zadawa zelen4uka
+        public Vegetable vegetable 
+        {
+            get { return _vegetable; }
+            set { _vegetable = value; }
+        }
+
+
+
+    }
+}
