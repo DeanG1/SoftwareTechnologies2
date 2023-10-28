394bdb57448f0a7a95b3e039ba77ce3c060edb44
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-28 19:01:51 +0300

    Add files via upload

---
+++ Cucumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegetablePriceobserver
+{
+    class Cucumber : Vegetable //ConcreteSubject1
+    {
+        public Cucumber(string vegetableName, double price)
+            : base(vegetableName, price)
+        {
+
+        }
+
+    }
+}
