e6898856b5b4ab367faa92bd3bfb0708999440b8
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-28 19:02:12 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
-﻿namespace StrategyNightBar
+﻿using VegetablePriceobserver;
+
+namespace VegetablePriceObserver 
 {
-    class Program
+    class Program 
     {
-        static void Main(string[] args){
-            Drink drink1 = new Drink(5, 1);
-            Drink drink2 = new Drink(10, 2);
+        static void Main(string[] args) 
+        
+        {
+            Cucumber cucumber = new Cucumber("Cucumber", 02.10);
+            cucumber.Attach(new Restaurant("Odeon"));
+            cucumber.Attach(new Restaurant("Nicole"));
+            cucumber.Attach(new Restaurant("Victoria"));
 
-            Order order = new Order();
-            order.AddDrink(drink1); 
-            order.AddDrink(drink2); 
+            Cabbage cabbage = new Cabbage("Cabbage", 01.05);
+            cabbage.Attach(new Restaurant("Odeon"));
+            cabbage.Attach(new Restaurant("Nicole"));
+            cabbage.Attach(new Restaurant("Victoria"));
 
-            NightBarStrategy strategy1 = new DayStrategy();
-            strategy1.Calculate(order.TotalAmount());
+            Tomato tomato = new Tomato("Tomato", 01.95);
+            tomato.Attach(new Restaurant("Odeon"));
+            tomato.Attach(new Restaurant("Nicole"));
+            tomato.Attach(new Restaurant("Victoria"));
 
-            NightBarStrategy strategy2 = new NormalStrategy();
-            strategy2.Calculate(order.TotalAmount());
+            cucumber.price = 02.25;
+            cucumber.price = 02.15;
+            cucumber.price = 02.20;
+
+            cabbage.price = 01.10;
+            cabbage.price = 01.15;
+            cabbage.price = 01.10;
+
+            cucumber.price = 02.10;
+            cucumber.price = 02.25;
+            cucumber.price = 02.20;
+
+            Console.ReadKey();
         }
     }
 }
