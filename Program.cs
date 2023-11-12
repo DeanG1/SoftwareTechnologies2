0e7ce5d90dca57505e49eedb9667d22b68640c3d
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-12 15:47:03 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,16 @@
-﻿using VegetablePriceobserver;
-
-namespace VegetablePriceObserver 
+﻿namespace CommandStructure
 {
-    class Program 
+    internal class Program
     {
-        static void Main(string[] args) 
-        
-        {
-            Cucumber cucumber = new Cucumber("Cucumber", 02.10);
-            cucumber.Attach(new Restaurant("Odeon"));
-            cucumber.Attach(new Restaurant("Nicole"));
-            cucumber.Attach(new Restaurant("Victoria"));
+        static void Main(string[] args)
+        {                                                                                                                                                                                                                                                                                                                                                                                                                                         
+            Invoker invoker = new Invoker();
+            invoker.SetOnStart(new SimpleCommand("Say Hi!"));
+            Receiver receiver = new Receiver();
+            invoker.SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
 
-            Cabbage cabbage = new Cabbage("Cabbage", 01.05);
-            cabbage.Attach(new Restaurant("Odeon"));
-            cabbage.Attach(new Restaurant("Nicole"));
-            cabbage.Attach(new Restaurant("Victoria"));
-
-            Tomato tomato = new Tomato("Tomato", 01.95);
-            tomato.Attach(new Restaurant("Odeon"));
-            tomato.Attach(new Restaurant("Nicole"));
-            tomato.Attach(new Restaurant("Victoria"));
-
-            cucumber.price = 02.25;
-            cucumber.price = 02.15;
-            cucumber.price = 02.20;
-
-            cabbage.price = 01.10;
-            cabbage.price = 01.15;
-            cabbage.price = 01.10;
-
-            cucumber.price = 02.10;
-            cucumber.price = 02.25;
-            cucumber.price = 02.20;
-
-            Console.ReadKey();
+            invoker.DoSomethingImportant();
         }
+
     }
 }
