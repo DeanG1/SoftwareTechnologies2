cc09283594b8edd3059a2e375c6fe06d73157527
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-13 21:55:24 +0200

    Add files via upload

---
+++ Coffee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotDrink
+{
+     class Coffee : HotDrink     //ConcreteClass1
+    {
+        public override void AddMainIngredient()
+        {
+            Console.WriteLine("Adding Coffee...");
+        }
+        public override void AddSupplement()
+        {
+            Console.WriteLine("Adding cream, sugar and milk...");
+        }
+
+    }
+}
