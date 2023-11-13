53bb12f5daf2aba2789dd4130804df429ffba4b4
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-13 21:55:33 +0200

    Add files via upload

---
+++ HotDrink.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotDrink
+{
+    abstract class HotDrink  //AbstractClass
+    {
+        public void MakeDrink()  //TemplateMethod
+        {
+            BoilWater();
+            AddMainIngredient();      //PrimitiveOperation1
+            PourInCup();
+            AddSupplement();            //PrimitiveOperation2
+        }
+
+        public void BoilWater() 
+        {
+            Console.WriteLine("Water is ready!");
+        }
+
+        public abstract void AddMainIngredient();
+
+        public void PourInCup() {
+            Console.WriteLine("Drink is in cup!");
+        }
+        public abstract void AddSupplement();
+    }
+}
