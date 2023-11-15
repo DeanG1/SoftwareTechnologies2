2ac1a6788e2020a40f2968e1d9bf7f187cc76b9f
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-15 22:45:37 +0200

    Add files via upload

---
+++ MeatPizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizzaApp_2101681075
+{
+	class MeatPizza : Pizza
+	{
+		public override void AddIngredients()
+		{
+			Console.WriteLine("Add ham, becon , chicken fillet, sausage!");
+		}
+	}
+}
