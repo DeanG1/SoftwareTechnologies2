97dc312f306b5c2b465aa9a9bc2eee297135b2b7
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-15 22:46:18 +0200

    Add files via upload

---
+++ VeganPizza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizzaApp_2101681075
+{
+	class VeganPizza : Pizza   //ConcreteClass1
+	{
+		public override void AddIngredients()
+		{
+			Console.WriteLine("Add tomato, onion, spinach, arugula, corn, pepper and peas!");
+		}
+	
+	}
+}
