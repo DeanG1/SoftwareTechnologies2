b4990b9a3f35c7ace069f420d0ac7895fb8f715c
Dido <100509456+Dido2002@users.noreply.github.com>
2023-11-15 22:45:47 +0200

    Add files via upload

---
+++ Pizza.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizzaApp_2101681075
+{
+	abstract class Pizza  //AbstractClass
+	{
+		public void MakePizza()  //TemplateMethod
+		{
+			MakeDough();
+			AddTomatoSauce();      //PrimitiveOperation1
+			AddCheese();
+			AddIngredients();            //PrimitiveOperation2
+		}
+
+		public void MakeDough()
+		{
+			Console.WriteLine("Dough is ready!");
+		}
+
+		public void AddTomatoSauce() {
+			Console.WriteLine("The tomato sauce is added!");
+		}
+
+		public void AddCheese()
+		{
+			Console.WriteLine("The cheese is added!");
+		}
+		public abstract void AddIngredients();
+	}
+}
